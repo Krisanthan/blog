939696cc0f75b5839677ff5b10d62b44dee58c8f
Krisanthan <krish.balakrishnan@mail.utoronto.ca>
2018-03-26 20:16:40 -0400

    added hello path

--- blog/urls.py
+++ blog/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from blog.blog.views import hello_world
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', views.hello_world),
 ]
